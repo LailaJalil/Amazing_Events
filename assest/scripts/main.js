23beaaed007ad189a04ce2a2a5e73d3de0f3ca83
LailaJalil <lailajalil@gmail.com>
2022-10-16 21:10:23 -0300

    filtro de cards index por checkbox y buscador

--- assest/scripts/main.js
+++ assest/scripts/main.js
@@ -1,84 +1,66 @@
-//impresión y filtro de cards
 let events = data.events
-let containerHome= document.getElementById("container-card-home")
-let text= document.getElementById("text-search-js")
-let btnSearch= document.getElementById("js-search")
-let checkbox= document.getElementById("js-checkbox")
-
-printCardFiltered(events, containerHome)
+let containerHome = document.getElementById("container-card-home")
+let text = document.getElementById("text-search-js")
+let btnSearch = document.getElementById("js-search")
+let checkBox = document.getElementById("js-checkbox") 
 
 
-// checkbox
-
-let categoryFiltrada = Array.from(new Set(events.map(event=> event.category)))
-check(categoryFiltrada)
-
-checkbox.addEventListener("change", (e)=>{
-   filtrarCheckbox(categoryFiltrada)
-})
-
-function filtrarCheckbox (array){
-    let checkBox= array.filter(check=>check.checked)
-    console.log(checkbox)
-    let category= checkBox.map(filtro=>filtro.value)
-    console.log(category.value);
-
-}
+//print cards
 
 
+printCardFiltered(events)
 
-//print category
 
-function check (array){
-    array.forEach(function(array){
-            checkbox.innerHTML +=`
-            <div class="form-check form-switch">
-             <label class="text-white form-check-label pe-2" for="${array}">${array}</label>
-             <input class=" form-check-input bg-danger check-box-js " type="checkbox"  for="${array}" value="${array}" name="${array}">
-             </div> 
-    `
-})}
-
+//print checks
+crearcheck(filtrarCheckboxes(events))
 
 //buscador
 
-// console.log(events);
-text.addEventListener("keyup", (e)=>{
-    containerHome.innerHTML =""
-    buscarPorTexto(text.value, events)
-
+//listeners
+//listener text search
+text.addEventListener("keyup", (e) => {
+    containerHome.innerHTML = ""
+    let filtroCheck= buscarPorCheckBoxes(events)
+    let filtroText= buscarPorTexto(text.value, filtroCheck)
+    printCardFiltered(filtroText) 
+    
 })
-btnSearch.addEventListener("click", (e)=>{
-  e.preventDefault()
-  containerHome.innerHTML =""
-  buscarPorTexto(text.value, events)
-  if (!buscarPorTexto(text.value, events)){
-    window.alert("Oops!Please try again with another search  and/or search for a specific keyword to get a faster and more accurate result ")
-}
-  
-})  
-//  console.log(events.category); /// me da undefined por eso no filtra
+
+//listener search btn
+btnSearch.addEventListener("click", (e) => {
+    e.preventDefault()
+    containerHome.innerHTML = ""
+    let filtroCheck= buscarPorCheckBoxes(events)
+    let filtroText= buscarPorTexto(text.value, filtroCheck)
+    printCardFiltered(filtroText) 
+ 
+})
+
+checkBox.addEventListener("change", (e) => {
+   
+    let filtroText= buscarPorCheckBoxes(events)
+    let filtroCheck= buscarPorTexto(text.value,filtroText)
+    printCardFiltered(filtroCheck)
+   
+    
+})
+
 
 //FUNCTION
 
-    function buscarPorTexto(texto,array){
-        let arrayFiltrado= array.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase()))
-        printCardFiltered(arrayFiltrado, containerHome)
-        
 
-    }
-
-    function crearCard(evento, elemento){
-        elemento.innerHTML += `
+//Cards
+function crearCard(array) {
+    containerHome.innerHTML += `
         
         <article class=" card rounded-4 p-0 pb-5 " style="width: 18rem;" id="card">
-        <img src="${evento.image}" alt="${evento.name}" class="rounded-top"  />
+        <img src="${array.image}" alt="${array.name}" class="rounded-top"  />
             <div class="card-body pb-0 mb-0">
-        <h5 class="card-title">${evento.name}</h5>
-        <p class="card-text">${evento.date}</p>
-        <p class="card-text">${evento.description}</p>
+        <h5 class="card-title">${array.name}</h5>
+        <p class="card-text">${array.date}</p>
+        <p class="card-text">${array.description}</p>
         <div class="d-flex flex-row justify-content-between pt-3">
-                            <p class="fw-bold">Price $ ${evento.price}</p>
+                            <p class="fw-bold">Price $ ${array.price}</p>
                             <a href="#" class="btn">More info</a>
                     </div>
             </div>
@@ -88,8 +70,66 @@ btnSearch.addEventListener("click", (e)=>{
     `
 }
 
-function printCardFiltered(events, elemento){
-    events.forEach(function(events){
-     crearCard(events,elemento)
+function printCardFiltered(array) {
+    array.forEach(function (array) {
+        crearCard(array)
     })
- }
+}
+
+
+
+//Checkboxes
+function filtrarCheckboxes(events) {
+    let filtroCategory = new Set(events.map(event => event.category))
+    return category = Array.from(filtroCategory)
+}
+
+
+function crearcheck(array) {
+
+    array.forEach(array => {
+        checkBox.innerHTML += `
+            <div class="form-check form-switch">
+             <label class="text-white form-check-label pe-2" for="${array}">${array}</label>
+             <input class=" form-check-input bg-danger check-box-js" type="checkbox"  for="${array}" value="${array}" name="${array}">
+             </div> 
+    `
+
+    })
+}
+
+/// filtrado x texto
+
+function buscarPorTexto(texto, array) {
+    
+        let arrayFiltrado = array.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase()))
+        return printCardFiltered(arrayFiltrado)
+    
+}
+
+//not found
+function notFound(){
+    containerHome.innerHTML= `
+    <div class="alert alert-secondary" role="alert">
+     Oops! Please change your search criteria
+     </div>
+     `
+     appendto(btnSearch)
+}
+
+function buscarPorCheckBoxes(array) {
+    let checkboxes = Array.from(document.querySelectorAll("input[type='checkbox']"))
+    let checkSelected = checkboxes.filter(evento => evento.checked)
+    let checkValue = checkSelected.map(evento => evento.value)
+    if (checkValue.length > 0) {
+        containerHome.innerHTML = ""
+        let checkFiltrado=array.filter(evento=>checkValue.includes(evento.category))
+        
+        return checkFiltrado
+    }
+    
+
+    return array
+}
+
+
