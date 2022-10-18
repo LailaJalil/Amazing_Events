f5597cc0aae84f2581f640f40e33af390baaad7b
LailaJalil <lailajalil@gmail.com>
2022-10-17 23:18:30 -0300

    filtros funcionando

--- assest/scripts/main.js
+++ assest/scripts/main.js
@@ -1,4 +1,5 @@
 let events = data.events
+console.log(events)
 let containerHome = document.getElementById("container-card-home")
 let text = document.getElementById("text-search-js")
 let btnSearch = document.getElementById("js-search")
@@ -10,7 +11,7 @@ let btnDark= document.getElementById("dark-mode-js")
 //print cards
 
 
-printCardFiltered(events)
+events.forEach(cardCreator)
 
 
 //print checks
@@ -24,7 +25,10 @@ text.addEventListener("keyup", (e) => {
     containerHome.innerHTML = ""
     let filtroCheck= buscarPorCheckBoxes(events)
     let filtroText= buscarPorTexto(text.value, filtroCheck)
-    printCardFiltered(filtroText)    
+    if(filtroText.length !==0){
+        containerHome.innerHTML=""
+    }
+    filtroText.forEach(cardCreator)
     
 })
 
@@ -33,26 +37,24 @@ btnSearch.addEventListener("click", (e) => {
     e.preventDefault()
     containerHome.innerHTML = ""
     let filtroCheck= buscarPorCheckBoxes(events)
-    console.log(filtroCheck);
     let filtroText= buscarPorTexto(text.value, filtroCheck)
-    if (filtroText = false){
-    printCardFiltered(filtroText)}
-    else{
-        return  notFound() + printCardFiltered(events)
+    if(filtroText.length !==0){
+        containerHome.innerHTML=""
     }
+    filtroText.forEach(cardCreator)
+    
     // if (filtroText != text.value){
     //     return notFound(alert) 
     // }
 })
 
 checkBox.addEventListener("change", (e) => {
-   
-    let filtroText= buscarPorCheckBoxes(events)
-    console.log(filtroText)
-    let filtroCheck= buscarPorTexto(text.value,filtroText)
-    console.log(filtroCheck)//undefined
-    printCardFiltered(filtroCheck)
-   
+    let filtroCheck= buscarPorCheckBoxes(events)
+    let filtroText= buscarPorTexto(text.value, filtroCheck)
+    if(filtroText.length !==0){
+        containerHome.innerHTML=""
+    }
+    filtroText.forEach(cardCreator)
     
 })
 
@@ -61,66 +63,59 @@ checkBox.addEventListener("change", (e) => {
 
 
 //Cards
-function crearCard(array) {
+
+function cardCreator (array){
+    
     containerHome.innerHTML += `
         
-        <article class=" card rounded-4 p-0 pb-5 " style="width: 18rem;" id="card">
-        <img src="${array.image}" alt="${array.name}" class="rounded-top"  />
-            <div class="card-body pb-0 mb-0">
-        <h5 class="card-title">${array.name}</h5>
-        <p class="card-text">${array.date}</p>
-        <p class="card-text">${array.description}</p>
-        <div class="d-flex flex-row justify-content-between pt-3">
-                            <p class="fw-bold">Price $ ${array.price}</p>
-                            <a href="#" class="btn">More info</a>
-                    </div>
-            </div>
-    </article>
- 
-    
-    `
+    <article class=" card rounded-4 p-0 pb-5 " style="width: 18rem;" id="card">
+    <img src="${array.image}" alt="${array.name}" class="rounded-top"  />
+        <div class="card-body pb-0 mb-0">
+    <h5 class="card-title">${array.name}</h5>
+    <p class="card-text">${array.date}</p>
+    <p class="card-text">${array.description}</p>
+    <div class="d-flex flex-row justify-content-between pt-3">
+                        <p class="fw-bold">Price $ ${array.price}</p>
+                        <a href="#" class="btn">More info</a>
+                </div>
+        </div>
+</article>
+
+
+`
 }
-
-function printCardFiltered(array) {
-    array.forEach(array=> crearCard(array))
-    
-    // array.forEach(function(array){
-    //     let printed= crearCard(array)
-    //     return printed
-    // })
-    // let print=array.forEach( function (array){ crearCard(array)})
-    // return print
-    
-}
-
-
+      
 
 //Checkboxes
+
+
+function crearcheck(array) {
+    
+    array.forEach(array => {
+        checkBox.innerHTML += `
+        <div class="form-check form-switch">
+        <label class="text-white form-check-label pe-2" for="${array}">${array}</label>
+        <input class=" form-check-input bg-danger check-box-js" type="checkbox"  for="${array}" value="${array}" name="${array}">
+        </div> 
+        `
+        
+    })
+}
+
 function filtrarCheckboxes(events) {
     let filtroCategory = new Set(events.map(event => event.category))
     return category = Array.from(filtroCategory)
 }
-
-
-function crearcheck(array) {
-
-    array.forEach(array => {
-        checkBox.innerHTML += `
-            <div class="form-check form-switch">
-             <label class="text-white form-check-label pe-2" for="${array}">${array}</label>
-             <input class=" form-check-input bg-danger check-box-js" type="checkbox"  for="${array}" value="${array}" name="${array}">
-             </div> 
-    `
-
-    })
-}
-
 /// filtrado x texto
 
 function buscarPorTexto(texto, array) {
         
         let arrayFiltrado = array.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase())|| evento.price == texto)
-        return printCardFiltered(arrayFiltrado)
+        if(arrayFiltrado.length === 0) {
+            notFound()
+            return []
+        }
+        return arrayFiltrado
         
         // if (arrayFiltrado != texto){
         //     notFound()
@@ -131,26 +126,64 @@ function buscarPorTexto(texto, array) {
 
 //not found
 function notFound(){
-    error= document.getElementById("js-alert")
-    error.classList.remove("hide")
-    
-     
+ 
+    let image = document.createElement('img')
+    image.src  = "assest/images/not_found.jpg"
+     containerHome.innerHTML = `
+     <div class="card text-dark notfound">
+     <p class="card-text"><small>Your word <span class="fw-bold">"${text.value}"</span> didn´t bring a match.</small></p> 
+   </div>
+     `
+      
 }
 
 
 function buscarPorCheckBoxes(array) {
     let checkboxes = Array.from(document.querySelectorAll("input[type='checkbox']"))
+
     let checkSelected = checkboxes.filter(evento => evento.checked)
+
     let checkValue = checkSelected.map(evento => evento.value)
+    
     if (checkValue.length > 0) {
         containerHome.innerHTML = ""
         let checkFiltrado=array.filter(evento=>checkValue.includes(evento.category))
         
         return checkFiltrado
+       
     }
     
 
     return array
 }
+// console.log(document.body.classList);
+// let darkMode= document.getElementById("dark-mode-js")
+// console.log(darkMode);
+// darkMode.addEventListener("click",(e)=>{
+ 
+// })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
