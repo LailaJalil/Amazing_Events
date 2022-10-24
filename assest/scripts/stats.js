033001f11287355345ca8ba2dfb35363569dfc3c
LailaJalil <lailajalil@gmail.com>
2022-10-23 22:41:55 -0300

    funciones para sumar totales de asistencia y porcentaje de 1 categoria, queda ver como hacer de todas juntas para imprimirlo

--- assest/scripts/stats.js
+++ assest/scripts/stats.js
@@ -1,4 +1,13 @@
 let containerStats= document.getElementById("containerStats")
+let tableNodelist= document.querySelectorAll(".category")
+let tableCategory= [...tableNodelist]
+let upComingCategory= tableCategory[0]
+let pastCategory= tableCategory[1]
+let highestA = document.getElementById("highestAttendance")
+let lowestA = document.getElementById("lowestAttendance")
+let highestCapacity = document.getElementById("largerCapacity")
+let table2= document.getElementById("table2-js")
+let table3= document.getElementById("table3-js")
 
 async function stats(){
     try{
@@ -6,17 +15,9 @@ async function stats(){
         let data = await info.json()
         let events= data.events
         let eventsCopy= [...events]
-        assistance=sortBy(eventsCopy,["assistance"])
+        agregarPropiedades(eventsCopy,["assistance"])
+        
         //Event with larger capacity
-        let highCapacity= sortBy(eventsCopy,["assistance"])[0].name
-        //Events with the highest percentage of attendance
-        highestAttendance= assistance[0].name
-        //Events with the lowest percentage of attendance
-        lowestAttendance= assistance[assistance.length-1].name
-        //print en tabla
-        dataPrint(highestAttendance, highestA)
-        dataPrint(lowestAttendance, lowestA)
-        dataPrint(highCapacity,highestCapacity)  
         
     }
     catch (error){
@@ -36,12 +37,13 @@ async function upComingAndPast (){
         let infoCategory = await fetch("https://mind-hub.up.railway.app/amazing?category")
         let datos = await infoCategory.json()
         let eventCategory = datos.events
+        console.log(eventCategory)
         //categorías ordenadas x mayor ganancia
-        sortBy(upComingCopy, ["revenue"])
-        let categoryUp = [...new Set(upComingCopy.map((event)=> event.category))]
-        categoryPrint(categoryUp,upComingCategory)
+        // sortBy(upComingCopy, ["revenue"])
+        // let categoryUp = [...new Set(upComingCopy.map((event)=> event.category))]
+        
     //Revenues
-
+            
 //Percentage of attendance
         //PastEvents
         let answerPast = await fetch("https://mind-hub.up.railway.app/amazing?time=past&order=desc")
@@ -49,15 +51,29 @@ async function upComingAndPast (){
         let past= eventPast.events
         let pastCopy= [...past]
         agregarPropiedades( pastCopy,["assistance"])   
-        
-        //categorias ordenadas x mayor ganancia Past
-        sortBy(pastCopy, ["revenue"])
-        let categoryPast = [...new Set(pastCopy.map((event)=> event.category))]
-        categoryPrint(categoryPast,pastCategory)
-        /// por categorias
-        
-        
+        let assistance=sortBy(pastCopy,["assistance"])
+        let highCapacity= sortBy(pastCopy,["assistance"])[0]
+        //Events with the highest percentage of attendance
+        let highestAttendance= assistance[0]
+       
+        //Events with the lowest percentage of attendance
+        let lowestAttendance= assistance[assistance.length-1]
      
+        //print en tabla1
+        dataPrint(highestAttendance, highestA, ["percentage"], "%" )
+        dataPrint(lowestAttendance, lowestA,["percentage"], "%")
+        dataPrint(highCapacity,highestCapacity, ["capacity"])  
+        categoryRevenues(pastCopy,"cinema")
+      
+        //categorias ordenadas x mayor ganancia Past
+        // sortBy(pastCopy, ["revenue"])
+        let categoryPast = [...new Set(pastCopy.map((event)=> event.category))]
+     
+        /// por categorias
+        allCategories(categoryPast, pastCopy)
+        console.log(allCategories(categoryPast, pastCopy));
+        sumTable(categoryPast, pastCopy, "assistance")
+        // ME QUEDA VER COMO PASO ESE ARRAR DE ARRAY DE OBJETOS POR EL SUMTABLE
     }
     catch(error){
         console.log(error)
@@ -87,7 +103,8 @@ upComingAndPast()
 //FUNCTIONS
 
 //Revenues
-function categoryRevenues(array,keyword,){
+function categoryRevenues(array,keyword){
+    
     let revenues = [...array].filter(evento => evento.category.toLowerCase().includes(keyword.toLowerCase()))
     revenues.sort((evento2,evento1)=> evento2.ganancia-evento1.ganancia)
   
@@ -99,21 +116,49 @@ function agregarPropiedades(array,[propiedad]){
         evento.percentage = Number((100 *evento[propiedad] /evento.capacity).toFixed())
     })
 }
-//ordenar por prodiedades
+//ordenar por prodiedades 
 function sortBy(array, [propiedad]){
   let ordenado=  array.sort((evento1,evento2)=> evento2[propiedad]-evento1[propiedad])
   return ordenado
 
 }
 
-// function sumarGanancias(array, [propiedad]){
-//     array.reduce((funcion, estadoInicial)=>{
-//         [propiedad]
-//     })
-
+function sumTable(array, categorias, propiedad){
+ allCategories(categorias, array)
+let start= {
+    category: "",
+    revenue: 0,
+    capacity: 0,
+    [propiedad]: 0,
     
-// }
+}
+let sum= array.reduce((elemento1,elemento2)=>{
+    return {
+       category: elemento1.category,
+       revenue: elemento1.revenue + elemento2.revenue,
+       capacity: elemento1.capacity + elemento2.capacity,
+       [propiedad]: elemento1[propiedad]+ elemento2[propiedad]   
+    } 
+},start)
 
+sum.percentageTotal= (100 * sum[propiedad] / sum.capacity).toFixed(2)
+
+return sum
+
+}
+function allCategories(categorias, array){
+  let joinedCategories= categorias.map(categorias=>{
+    let filtrado= array.filter(array=>array.category== categorias)
+    return filtrado
+  })
+ return joinedCategories
+}
+// let stats = categories.map(cat => {
+//     let filter = events.filter(event => event.category===cat)
+//     return reduceStats(filter,property)
+// })
+// printTable2(stats,id)
+// }
 //Percentage
 
 //highest percentage of attendance Past
@@ -122,29 +167,35 @@ function sortBy(array, [propiedad]){
 
 ///print categories
 
-let tableNodelist= document.querySelectorAll(".category")
-let tableCategory= [...tableNodelist]
-let upComingCategory= tableCategory[0]
-let pastCategory= tableCategory[1]
-let highestA = document.getElementById("highestAttendance")
-let lowestA = document.getElementById("lowestAttendance")
-let highestCapacity = document.getElementById("largerCapacity")
-console.log(highestA)
-function categoryPrint(array,place){
-    array.forEach((array) => {
-    let tr= document.createElement("tr")
-    tr.className ="category td"
-    tr.innerHTML +=`${array}`
-    place.appendChild(tr)
+
+
+function tablePrint(array,id){
+document.querySelector(`#${id}`).innerHTML =
+        
+         `
+        <td>${array.category}</td>
+        <td>${array.revenue}</td>
+        <td>${array.assistance}</td>
+        `
     
-})
+    
+
+    // array.forEach((array) => {
+    // let tr= document.createElement("tr")
+    // tr.className ="category td"
+    // tr.innerHTML +=`${array}`
+    // place.appendChild(tr)
+    
+
 }
 //PRINT INFO
-function dataPrint(elemento, place){
+function dataPrint(elemento, place,propiedad, valor){
 
     let tr= document.createElement("tr")
-    tr.innerHTML +=`${elemento}`
+    tr.innerHTML +=`${elemento.name}`
     place.appendChild(tr)
-    
+    let tr2= document.createElement("tr")
+    tr2.innerHTML +=`${elemento[propiedad] + [valor]} `
+    tr.appendChild(tr2)
 
 }
